3106eed6c5c59c933032a90400bc8824bcb70931
Željko Jukić <104694437+zeljko-ju@users.noreply.github.com>
2022-05-06 20:10:47 +0200

    Create domacazadaca1.sql

---
+++ domacazadaca1.sql
@@ -0,0 +1,12 @@
+# linija koja počne s hash (shift + 3) je komentar
+# Ovo je SQL skripta
+# naredba za izvođenje
+# C:\xampp\mysql\bin\mysql -uroot < C:\Users\php\Documents\GitHub\Jukic-zadaca\domacazadaca1.sql
+
+
+drop database if exists domacazadaca1;
+create database domacazadaca1;
+use domacazadaca;
+
+
+
