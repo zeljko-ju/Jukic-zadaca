67f978e878be90d736bb22bb365091b3f0f7d4f7
Željko Jukić <104694437+zeljko-ju@users.noreply.github.com>
2022-05-07 13:24:37 +0200

    Update domacazadaca1.sql

--- domacazadaca1.sql
+++ domacazadaca1.sql
@@ -7,8 +7,8 @@ create database domacazadaca1;
 use domacazadaca1;
 
 
-#frizerski #salon za uljepsavanje #udruga za zastitu zivotinja 
-show tables;
+#frizerski salon | salon za uljepsavanje | udruga za zastitu zivotinja | postolarska radnja | urar |
+
 
 create table djelatnici (
     ime varchar(50),
@@ -50,4 +50,38 @@ create table izlozbe (
     ime_autora varchar(50),
     br_djela varchar(50),
     kustos varchar(50)
+);
+#samostan
+create table samostan(
+    ime varchar(50),
+    prezime varchar(50),
+    titula varchar(50)
+);
+create table casne_sestre(
+    ime varchar(50),
+    prezime varchar(50),
+    red varchar(50)
+);
+#taxi sluzba
+create table vozac(
+    ime varchar(50),
+    prezime varchar(50),
+    vozilo_br varchar(50),
+    recenzija varchar(50)
+
+);
+#postolarska radnja
+create table postolar(
+    ime varchar(50),
+    prezime varchar(50),
+    kontakt varchar(50),
+    adresa varchar(50)
+);
+#urar
+create table urar(
+    ime varchar(50),
+    prezime varchar(50),
+    usluga varchar(50),
+    kontakt varchar(50)
+
 );
