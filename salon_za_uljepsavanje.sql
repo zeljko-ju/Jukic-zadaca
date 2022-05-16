c1c8615ed268f7d1d98152b2df3a5372cbd79d3d
Željko Jukić <104694437+zeljko-ju@users.noreply.github.com>
2022-05-16 07:02:22 +0200

    salon za uljepsavanje +  sticenici

--- salon_za_uljepsavanje.sql
+++ salon_za_uljepsavanje.sql
@@ -1,18 +1,79 @@
+#C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\php\Documents\GitHub\Jukic-zadaca\salon_za_uljepsavanje.sql
+
+
 
 drop database if exists salon_za_uljepsavanje;
 create database salon_za_uljepsavanje;
 use salon_za_uljepsavanje;
 
-create table djelatnice (
-    ime varchar(50),
-    prezime varchar(50),
+create table djelatnica (
+    sifra int not null primary key auto_increment,
+    osoba int not null
+      
+);
+
+create table korisnik (
+    sifra int not null primary key auto_increment,
+    osoba int not null,
+    djelatnica int,
+    usluga int,
+    termin datetime
+);
+
+create table usluga (
+    sifra int not null primary key auto_increment,
     usluga varchar(50),
-    termin datetime
-
-
+    cijena decimal(18,2)
+  
 );
-create table korisnici (
+
+create table osoba (
+    sifra int not null primary key auto_increment,
     ime varchar(50),
-    prezime varchar (50),
-    termin datetime
+    prezime varchar(50)
+
 );
+
+alter table djelatnica add foreign key(osoba) references osoba(sifra);
+alter table korisnik add foreign key(osoba) references osoba(sifra);
+
+
+
+alter table korisnik add foreign key(usluga) references usluga(sifra);
+alter table korisnik add foreign key(djelatnica) references djelatnica(sifra);
+
+
+insert into osoba (ime, prezime)
+values ('Marza', 'Ivić');
+
+insert into osoba (ime, prezime)
+values ('Petra', 'Perić');
+
+insert into osoba (ime, prezime)
+values ('Svjetlana', 'Perić');
+
+insert into osoba (ime, prezime)
+values ('Zvonko', 'Marić');
+
+insert into osoba (ime, prezime)
+values ('Pero', 'Zdravkić');
+
+insert into usluga (usluga, cijena)
+values ('opuštajuća masaža', '150');
+
+insert into usluga (usluga, cijena)
+values ('depilacija', '100');
+
+insert into djelatnica (osoba)
+values (2);
+
+insert into korisnik (osoba, djelatnica, usluga, termin)
+values (4, 1, 1, '2022-05-16 17:00:00');
+
+insert into korisnik (osoba, djelatnica, usluga, termin)
+values (3, 1, 2, '2022-05-16 14:30:00');
+
+insert into korisnik (osoba, djelatnica, usluga, termin)
+values (2, 1, 1, '2022-05-16 14:30:00');
+
+
--- udruga_za_zastitu_zivotinja.sql
+++ udruga_za_zastitu_zivotinja.sql
@@ -1,16 +1,50 @@
+#C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8 < C:\Users\php\Documents\GitHub\Jukic-zadaca\udruga_za_zastitu_zivotinja.sql
+
+
 
 drop database if exists udruga_za_zastitu_zivotinja;
 create database udruga_za_zastitu_zivotinja;
 use udruga_za_zastitu_zivotinja;
 
-create table djelatnici (
+create table djelatnik (
+    sifra int not null primary key auto_increment,
     ime varchar(50),
     prezime varchar(50),
+    sticenik int,
     broj_sticenika varchar(50)
 );
 
-create table sticenici (
-    ime varchar(50),
+create table sticenik (
+    sifra int not null primary key auto_increment,
+    sticenik varchar(50),
+    djelatnik int,
     vrsta_zivotinje varchar(50),
     pasmina varchar(50)
 );
+
+
+create table prostor (
+    sifra int not null primary key auto_increment,
+    sticenik int not null,
+    broj_prostora varchar(50)
+
+);
+
+alter table sticenik add foreign key(djelatnik) references djelatnik(sifra);
+alter table prostor  add foreign key(sticenik) references sticenik(sifra);
+
+
+insert into sticenik (sticenik, vrsta_zivotinje, pasmina)
+values ('Bobi', 'pas', 'mješanac');
+
+insert into sticenik (sticenik, vrsta_zivotinje, pasmina)
+values ('Ares', 'pas', 'staford');
+
+insert into sticenik (sticenik, vrsta_zivotinje, pasmina)
+values ('Kitty', 'mačka', 'mjesanac');
+
+insert into djelatnik (ime, prezime, sticenik, broj_sticenika)
+values ('Marko', 'Marković', 1, '5');
+
+
+
