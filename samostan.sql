2a56ef7928532be31f15cd25d5ed39b6f67cb2b1
Željko Jukić <104694437+zeljko-ju@users.noreply.github.com>
2022-05-21 20:47:17 +0200

    kreiran ER samostana

--- samostan.sql
+++ samostan.sql
@@ -1,12 +1,41 @@
-drop database samostan if exists;
+#u samostanu se nalaze svećenici, svaki svećenik
+#je zaduzen za vise poslova, jedan posao moze obavljati vise svećenika, 
+#svaki svecenik ima nadređenog
+## C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8  < C:\Users\php\Documents\GitHub\Jukic-zadaca\samostan.sql
+
+drop database if exists samostan;
 create database samostan;
 use samostan;
 
-create table svecenici (
+create table svecenik (
+    sifra int not null primary key auto_increment,
     ime varchar(50),
     prezime varchar(50),
-    red varchar(50),
-    zaduzenje varchar(50), 
-    nadredeni varchar(50)
+    zaduzenje int, 
+    nadredeni int
 
 );
+
+create table nadredeni (
+    sifra int not null primary key auto_increment,
+    ime varchar(50),
+    prezime varchar(50)
+
+);
+
+create table zaduzenje (
+    sifra int not null primary key auto_increment,
+    svecenik int, 
+    posao int, 
+    termin datetime
+    
+);
+
+create table posao (
+    sifra int not null primary key auto_increment,
+    opis_posla varchar(50)
+);
+
+alter table svecenik add foreign key (nadredeni) references nadredeni(sifra);
+alter table zaduzenje add foreign key (svecenik) references svecenik(sifra);
+alter table zaduzenje add foreign key (posao) references posao(sifra);
