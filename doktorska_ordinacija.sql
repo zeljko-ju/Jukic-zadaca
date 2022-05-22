a7d2a552fb095f975a5e53f5c62f433a67b4a64c
Željko Jukić <104694437+zeljko-ju@users.noreply.github.com>
2022-05-22 12:06:48 +0200

    dodatna vjezba ER
    ER vjezba, slijedi delete, update i select

--- doktorska_ordinacija.sql
+++ doktorska_ordinacija.sql
@@ -3,15 +3,61 @@ create database doktorska_ordinacija;
 use doktorska_ordinacija;
 
 create table doktor (
+    sifra int not null primary key auto_increment,
     ime varchar(50),
     prezime varchar(50),
-    specijalizacija varchar(50),
-    dezurstvo datetime
+    specijalizacija varchar(50), 
+    sestra int
+    
 );
 
-create table sestre(
+create table termin (
+    sifra int not null primary key auto_increment,
+    datum_vrijeme datetime,
+    doktor int
+);
+
+create table pacijent (
+    sifra int not null primary key auto_increment,
     ime varchar(50),
     prezime varchar(50),
-    odjel varchar(50),
-    dezurstvo datetime
+    broj_knjizice char(13),
+    termin int
 );
+
+create table sestra(
+    sifra int not null primary key auto_increment,
+    ime varchar(50),
+    prezime varchar(50),
+    zaduzenje varchar(50)
+);
+
+alter table termin add foreign key (doktor) references doktor(sifra);
+alter table pacijent add foreign key (termin) references termin(sifra);
+
+alter table doktor add foreign key (sestra) references sestra(sifra);
+
+#unošenje vrijednosti/test funkcionalnosti
+#doktor
+insert into doktor (sifra, ime, prezime, specijalizacija, sestra)
+	values (null, 'Janko', 'Maričević', 'kirurg', 1),
+	(null, 'Stjepan', 'Janjić', 'ortoped', 2);
+select * from doktor;
+#sestre
+insert into sestra (sifra, ime, prezime)
+	values (null, 'Marija', 'Jasnić'),
+	(null, 'Maja', 'Huljić');
+
+#termin
+insert into termin (sifra, datum_vrijeme, doktor)
+	values (null, '2022-05-23 07:00:00', 1),
+	(null, '2022-05-23 09:00:00', 2), 
+	(null, '2022-05-23 11:00:00', 1);
+#pacijenti
+insert into pacijent (sifra, ime, prezime, termin)
+	values(null, 'Ante', 'Perić',1),
+	(null, 'Mate', 'Jamić', 2), 
+	(null, 'Ante', 'Posavec', 3);
+	
+select * from pacijent;
+select * from termin;
--- taxi_sluzba.sql
+++ taxi_sluzba.sql
@@ -3,16 +3,48 @@ create database taxi_sluzba;
 use taxi_sluzba;
 
 create table vozac (
+    sifra int not null primary key auto_increment,
     ime varchar(50),
     prezime varchar(50),
-    vozilo_br varchar(50),
-    recenzija varchar(2),
-    br_prevezenih_putnika varchar(5)
+    marka_vozila varchar(50),
+    registarska_oznaka varchar(50),
+    broj_vozila decimal(3),
+    recenzija decimal(2)
 );
 
-create table korisnici (
+
+
+create table voznja (
+    sifra int not null primary key auto_increment,
+    vozac int, 
+    vrijeme datetime
+);
+
+create table putnik (
+    sifra int not null primary key auto_increment,
     ime varchar(50),
-    lokacija varchar(50),
-    odrediste varchar (50)
-
+    prezime varchar(50),
+    loyaty_klub boolean,
+    voznja int
 );
+
+alter table voznja add foreign key (vozac) references vozac(sifra);
+alter table putnik add foreign key (voznja) references voznja(sifra);
+
+
+#test funkcionalnosti i ubacivanje vrijednosti
+#vozači
+insert into vozac (sifra, ime, prezime, marka_vozila, registarska_oznaka, broj_vozila, recenzija)
+	values (null, 'Marko', 'Kovač', 'Opel', 'ZG 6117-HH', 1, 10);
+	
+#putnici
+insert into putnik (sifra, ime, prezime, voznja, loyaty_klub)
+	values (null, 'Mario', 'Jović', 1, true),
+	(null, 'Petar', 'Dujić',1, false),
+	(null, 'Stanko', 'Majić',2, false);
+#voznje
+insert into voznja (sifra, vozac,vrijeme)
+	values(null, 1,'2022-05-22 10:40:00'),
+	(null, 1, '2022-05-22 07:45:00');
+
+select * from putnik;
