b724fd1dd47314393999999c1609c9680e75f851
Zeljko-ju <zeljac00@icloud.com>
2022-05-26 19:32:34 +0200

    update
    zavrsavanje svih ključeva te dodavanje po 3 vrijednosti, vrijeme trajanja 38 min ovaj update, ukupno 49.
    tip*
    bolje uocavaj syntax greske. Obrati bolje paznju na er i tip podatka koji je zadan!! Obje stvari su me koštale puno u ovom pokušaju.

--- zadatak1_mini_break_DZ.sql
+++ zadatak1_mini_break_DZ.sql
@@ -1,5 +1,5 @@
 #C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8  < C:\Users\php\Documents\GitHub\Jukic-zadaca\zadatak1_mini_break_DZ.sql
-#vrijeme start1 17:38  stop 17:49
+#vrijeme start1 17:38  stop 17:49 start2 18:50 end 19:28
 
 drop database if exists dz_vjezba1;
 create database dz_vjezba1;
@@ -59,5 +59,31 @@ create table svekar (
 );
 
 alter table zena add foreign key (sestra) references sestra(sifra);
+alter table sestra_svekar add foreign key (sestra) references sestra(sifra);
+alter table muskarac add foreign key (zena) references zena(sifra);
+alter table mladic add foreign key (muskarac) references muskarac(sifra);
+alter table sestra_svekar add foreign key (svekar) references svekar(sifra);
 
+insert into muskarac (sifra, bojaociju, hlace, marka, modelnaocala,zena)	
+	values (null, 'crna', 'levis', 15.78,'ray ban', 1),
+	(null, 'smeđa', 'tiger of sweeden', 16.13,'police', 1 ),
+	(null, 'plava', 'levis', 15.15,2) ;
+	
 
+insert into zena (sifra, hlace, kratkamajica)
+	values (null, 'levis','puma'), 
+	(null, 'tiger of sweeden', 'addidas'),
+	(null, 'nemam pojma', 'elviton');
+
+select * from muskarac;
+
+insert into sestra (sifra, introvertno, haljina, marka)
+	values(null, true, 'duga, svečana', '205.6');
+
+insert into svekar (sifra, bojaociju, prstena, dukserica, lipa)
+	values (null, 'smeđa', 27, 'plava', 6006.55);
+insert into sestra_svekar (sifra, sestra, svekar)
+	values (null, 1, 1),
+	(null, 1, 1), 
+	(null, 1,1);
+select * from sestra_svekar;
