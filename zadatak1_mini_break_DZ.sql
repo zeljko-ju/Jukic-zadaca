6450cbe2da79b1c1b44bde09d8af81d4da748e11
Zeljko-ju <zeljac00@icloud.com>
2022-05-26 22:39:36 +0200

    update zadaca
    definitivno lose odrađene select, update, i deleteza par redova vise od pola sata(37 min), vjezbati redosljed i paziti na greske, u sljedećem pokušaju!!

--- zadatak1_mini_break_DZ.sql
+++ zadatak1_mini_break_DZ.sql
@@ -1,5 +1,5 @@
 #C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8  < C:\Users\php\Documents\GitHub\Jukic-zadaca\zadatak1_mini_break_DZ.sql
-#vrijeme start1 17:38  stop 17:49 start2 18:50 end 19:28
+#vrijeme start1 17:38  stop1 17:49 start2 18:50 end2 19:28 start 3 22:00 end3 22:37
 
 drop database if exists dz_vjezba1;
 create database dz_vjezba1;
@@ -57,17 +57,37 @@ create table svekar (
     eura decimal (12, 7),
     majica varchar(35)
 );
+create table punac(
+    sifra int not null primary key auto_increment,
+    ogrlica int, 
+    gustoca decimal (14,9),
+    hlace varchar(41)
+);
+create table cura (
+    sifra int not null primary key auto_increment,
+    novcica decimal(16,5),
+    gustoca decimal (18,6),
+    lipa decimal(13,10),
+    ogrlica int,
+    bojakose varchar(38), 
+    suknja varchar(38),
+    punac int
+
+);
 
 alter table zena add foreign key (sestra) references sestra(sifra);
 alter table sestra_svekar add foreign key (sestra) references sestra(sifra);
 alter table muskarac add foreign key (zena) references zena(sifra);
 alter table mladic add foreign key (muskarac) references muskarac(sifra);
 alter table sestra_svekar add foreign key (svekar) references svekar(sifra);
+alter table cura add foreign key(punac) references punac(sifra);
+
+
 
 insert into muskarac (sifra, bojaociju, hlace, marka, modelnaocala,zena)	
 	values (null, 'crna', 'levis', 15.78,'ray ban', 1),
 	(null, 'smeđa', 'tiger of sweeden', 16.13,'police', 1 ),
-	(null, 'plava', 'levis', 15.15,2) ;
+	(null, 'plava', 'levis', 15.15, 'Hugo Boss',3) ;
 	
 
 insert into zena (sifra, hlace, kratkamajica)
@@ -77,6 +97,13 @@ insert into zena (sifra, hlace, kratkamajica)
 
 select * from muskarac;
 
+#U tablici cura postavite svim zapisima kolonu gustoca na vrijednost 
+#15,77. (4)
+#3. U tablici mladic obrišite sve zapise čija je vrijednost kolone kuna 
+#veće od 15,78. (4
+
+
+select * from cura;
 insert into sestra (sifra, introvertno, haljina, marka)
 	values(null, true, 'duga, svečana', '205.6');
 
@@ -87,3 +114,9 @@ insert into sestra_svekar (sifra, sestra, svekar)
 	(null, 1, 1), 
 	(null, 1,1);
 select * from sestra_svekar;
+
+select * from cura;
+#select naziv as smjer, 'Edunova' as skola from smjer;
+
+
+select 15.77 as gustoca from cura;
