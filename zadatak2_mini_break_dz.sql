34bed05ef7887978d755354929e3a97ab51b9971
Zeljko-ju <zeljac00@icloud.com>
2022-05-29 15:06:16 +0200

    zadatak 2
    kreirane sve tablice, veze i rješeni svi zadatci osim 5. prolazno vrijeme 42:00 min

---
+++ zadatak2_mini_break_dz.sql
@@ -0,0 +1,84 @@
+#C:\xampp\mysql\bin\mysql -uroot --default_character_set=utf8  < C:\Users\php\Documents\GitHub\Jukic-zadaca\zadatak2_mini_break_dz.sql
+
+drop database if exists dz2_minibreak;
+create database dz2_minibreak;
+use dz2_minibreak;
+
+
+create table decko (
+    sifra int not null primary key auto_increment,
+    indiferentno boolean,
+    vesta varchar(34),
+    asocijalno boolean not null
+
+);
+
+create table cura(
+    sifra int not null primary key auto_increment,
+    haljina varchar(33) not null,
+    drugiputa datetime not null,
+    suknja varchar(38),
+    narukvica int,
+    introvertno boolean,
+    majica varchar(40) not null,
+    decko int
+);
+
+create table neprijatelj(
+        sifra int not null primary key auto_increment,
+        majica varchar(32),
+        haljina varchar(43) not null,
+        lipa decimal(16,8),
+        modelnaocala varchar(49) not null,
+        kuna decimal(12,6) not null,
+        jmbag char(11), 
+        cura int
+);
+create table brat (
+    sifra int not null primary key auto_increment,
+    suknja varchar(47),
+    ogrlica int not null,
+    asocijalno boolean not null,
+    neprijatelj int not null
+);
+
+create table decko_zarucnica (
+    sifra int not null primary key auto_increment,
+    decko int not null, 
+    zarucnica int not null
+);
+
+create table zarucnica (
+    sifra int not null primary key auto_increment,
+    narukvica int,
+    bojakose varchar(37) not null,
+    novcica decimal(15,9),
+    lipa decimal(15,8) not null,
+    indiferentno boolean not null
+);
+
+
+create table prijatelj (
+    sifra int not null primary key auto_increment,
+    modelnaocala varchar(37),
+    treciputa datetime not null,
+    ekstroventno boolean not null,
+    prviputa datetime, 
+    svekar int not null
+);
+
+create table svekar (
+    sifra int not null primary key auto_increment,
+    stilfrizura varchar(48), 
+    ogrlica int not null, 
+    asocijalno boolean not null
+);
+
+
+alter table cura add foreign key (decko) references decko(sifra);
+alter table neprijatelj add foreign key (cura) references cura(sifra);
+alter table brat add foreign key (neprijatelj) references neprijatelj(sifra);
+alter table decko_zarucnica add foreign key (decko) references decko(sifra);
+alter table decko_zarucnica add foreign key (zarucnica) references zarucnica(sifra);
+
+alter table prijatelj add foreign key (svekar) references svekar(sifra);
