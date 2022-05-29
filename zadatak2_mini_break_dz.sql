9e73ed4549735ff511077b8444324952d4907cc4
Zeljko-ju <zeljac00@icloud.com>
2022-05-29 17:37:18 +0200

    update vjezba 2
    45 minuta samo zadatak 5, i dalje nije potpun, fali mi nekih stvari. provjeri sa nekim kako ga točno riješiti

--- zadatak2_mini_break_dz.sql
+++ zadatak2_mini_break_dz.sql
@@ -82,3 +82,70 @@ alter table decko_zarucnica add foreign key (decko) references decko(sifra);
 alter table decko_zarucnica add foreign key (zarucnica) references zarucnica(sifra);
 
 alter table prijatelj add foreign key (svekar) references svekar(sifra);
+
+
+insert into decko(sifra, indiferentno, vesta, asocijalno)
+	values (null, true, 'crna', false),
+		(null, true, 'šarena', false),
+		(null, true, 'bijela', true);
+		
+insert into zarucnica (sifra, bojakose, lipa,novcica, indiferentno)
+	values (null, 'crna', 167.55,200.57, true),
+	(null, 'crna', 111.55,155.155, true),
+	(null, 'plava', 197.55,304.5567, true);
+
+insert into decko_zarucnica (sifra, decko, zarucnica)
+	values (null, 1, 2),
+		(null, 3, 2),
+		(null, 1, 3);
+	
+select * from decko;	
+insert into cura (sifra, haljina, drugiputa, majica, decko)
+	values(null, 'svečana duga', '2022-03-03','crna',1),
+	(null, 'šarena', '2022-03-03','tamno plava',2),
+	(null, 'kratka bijeka', '2022-03-03','indigo',3);
+	
+insert into neprijatelj (sifra, haljina, modelnaocala, kuna, cura)
+	values (null, 'svijetla', 'sportske', 199.99, 1),
+	(null, 'čipkasta', 'Ray Ban', 999.99, 3),
+	(null, 'svijetla', 'moderne', 399.99, 2);
+insert into svekar (sifra, ogrlica, asocijalno)
+	values (null, 24, false);
+select* from prijatelj;
+insert into prijatelj (sifra, treciputa, ekstroventno, svekar)
+	values (null, '2022-01-01', false, 1);
+
+insert into brat (sifra, ogrlica, asocijalno, neprijatelj)
+    values (null, 111, true, 1);
+
+	update prijatelj set treciputa= '2022-04.30';
+	
+
+delete from brat where ogrlica != 14;
+
+select suknja from cura;
+
+select a.vesta, a.asocijalno 
+from
+decko a right join decko_zarucnica b 
+on a.sifra = b.decko;
+
+insert into brat (sifra,ogrlica, asocijalno, neprijatelj)
+	values (null, 144,true, 1);
+	
+
+
+select a.novcica, f.neprijatelj,
+'ba' as vesta
+from 
+zarucnica a right join decko_zarucnica b
+on a.sifra = b.zarucnica 
+inner join decko c
+on b.decko =c.sifra 
+inner join cura d
+on c.sifra = d.decko 
+inner join neprijatelj e 
+on d.sifra = e.cura
+left join brat f 
+on e.sifra = f.neprijatelj
+;
