2ea12049e3a2ce65dc31e8a120ec6e7a20a4a33a
Zeljko-ju <zeljac00@icloud.com>
2022-09-22 20:12:01 +0200

    CRUD ponavljanje sa nastave

--- www/app.hr/app/controller/SmjerController.php
+++ www/app.hr/app/controller/SmjerController.php
@@ -3,12 +3,158 @@
 class SmjerController extends AutorizacijaController
 {
 
-   private $phtml = "privatno"
-   DIRECTORY_SEPARATOR = 'privatno' . DIRECTORY_SEPARATOR;
+    private $phtmlDir = 'privatno' . 
+        DIRECTORY_SEPARATOR . 'smjerovi' .
+        DIRECTORY_SEPARATOR;
+
+    private $smjer=null;
+    private $poruka='';
 
     public function index()
     {
-        $this->render($this->phtmlDir.'read',[
-        'smjerovi'=> Smjer::read()]
+
+        //
+        $nf = new NumberFormatter("hr-HR", \NumberFormatter::DECIMAL);
+        $smjerovi = Smjer::read();
+        foreach($smjerovi as $s){
+            $s->cijena = $nf->format($s->cijena);
+        }
+
+        $this->view->render($this->phtmlDir . 'read',[
+            'smjerovi' => $smjerovi
+        ]);
     }
+
+    public function promjena($sifra)
+    {
+        if(!isset($_POST['naziv'])){
+
+            $smjer = Smjer::readOne($sifra);
+            if($smjer==null){
+                header('location: ' . App::config('url') . 'smjer');
+            }
+
+            $this->view->render($this->phtmlDir . 'update',[
+                'smjer' => $smjer,
+                'poruka' => 'Promjenite podatke'
+            ]);
+            return;
+        }
+
+        $this->smjer = (object) $_POST;
+        $this->smjer->sifra=$sifra;
+        $this->smjer->certificiran = isset($_POST['certificiran']);
+
+        if($this->kontrolaPromjena()){
+            Smjer::update((array)$this->smjer);
+            header('location: ' . App::config('url') . 'smjer');
+            return;
+        }
+
+        $this->view->render($this->phtmlDir . 'update',[
+            'smjer'=>$this->smjer,
+            'poruka'=>$this->poruka
+        ]);
+
+
+    }
+
+    public function brisanje($sifra)
+    {
+
+        $smjer = Smjer::readOne($sifra);
+        if($smjer==null){
+            header('location: ' . App::config('url') . 'smjer');
+        }
+
+       
+
+        if(!isset($_POST['obrisi'])){
+            $this->view->render($this->phtmlDir . 'delete',[
+                'smjer' => $smjer,
+                'brisanje'=>Smjer::brisanje($sifra), 
+                'poruka' => 'Detalji smjera za brisanje'
+            ]);
+            return;
+        }
+
+        Smjer::delete($sifra);
+      
+        header('location: ' . App::config('url') . 'smjer');
+        
+
+    }
+
+    public function novi()
+    {
+        if(!isset($_POST['naziv'])){
+            // došao s HTTP GET method
+            $this->pripremiSmjer();
+            $this->view->render($this->phtmlDir . 'create',[
+                'smjer'=>$this->smjer,
+                'poruka'=>'Popunite sve podatke'
+            ]);
+            return; // short curcuiting
+        }
+         
+        // dođao s HTTP POST method
+        $this->smjer = (object) $_POST;
+        $this->smjer->certificiran = isset($_POST['certificiran']);
+    
+        if($this->kontrolaNovi()){
+            Smjer::create((array)$this->smjer);
+            header('location: ' . App::config('url') . 'smjer');
+            return;
+        }
+
+        $this->view->render($this->phtmlDir . 'create',[
+            'smjer'=>$this->smjer,
+            'poruka'=>$this->poruka
+        ]);
+        
+    }
+
+    private function kontrolaNovi()
+    {
+        return $this->kontrolaNaziv() && $this->kontrolaCijena();
+    }
+
+   
+
+    private function kontrolaPromjena()
+    {
+        return $this->kontrolaNaziv();
+    }
+
+    private function kontrolaNaziv()
+    {
+        if(strlen($this->smjer->naziv)===0){
+            $this->poruka = 'Naziv obavezno';
+            return false;
+        }
+        return true;
+    }
+
+    private function kontrolaCijena()
+    {
+        $broj = (float)$this->smjer->cijena;
+        if($broj<=0){
+            $this->poruka='Cijena mora biti veća od nule (0)';
+            $this->smjer->cijena=0;
+            return false;
+        }
+        return true;
+    }
+    
+
+    private function pripremiSmjer()
+    {
+        $this->smjer=new stdClass();
+        $this->smjer->naziv='';
+        $this->smjer->cijena='';
+        $this->smjer->trajanje='';
+        $this->smjer->upisnina='';
+        $this->smjer->certificiran=false;
+    }
+
 }
--- www/app.hr/app/model/smjer.php
+++ www/app.hr/app/model/smjer.php
@@ -13,16 +13,47 @@ class Smjer
             return $izraz ->fetchAll();
 
     }
-    public static function create()
+    public static function create($smjer)
     {
+        $veza = DB::getInstance();
+        $izraz= $veza->prepare('
+        insert into 
+    smjer(sifra,naziv,cijena,trajanje,
+    upisnina,certificiran)
+    values (null,:naziv,:cijena,:trajanje,
+    :upisnina,:certificiran);
         
+        ');
     }
     public static function update()
     {
+        $veza = DB::getInstance();
+        $izraz= $veza->prepare('
+         update smjer set
+         naziv=:naziv, 
+         cijena=:cijena,
+         trajanje=:trajanje,
+         upisnina=:upisnina,
+         certificiran=:certiiciran
+         where sifra=:sifra
         
+        ');  
     }
-    public static function delete()
+
+    select count(*) from grupa where smjer=:stream_filter_append
+
+    $izraz->ecevute ([
+        'sifra=>$sifra
+        '
+    ]);
+    public static function delete($sifra)
     {
+        $veza = DB::getInstance();
+        $izraz= $veza->prepare('
+         
+         delete from smjer where sifra=:sifra
         
+        ');  
+        izraz->execute
     }
 }
